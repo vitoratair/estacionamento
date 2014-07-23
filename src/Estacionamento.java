2dfc0b2ce226cf94d66a040b8fa551008bbab42b
Vitor Atair <vitor.ruts@gmail.com>
2014-07-23 07:55:16 -0300

    added new rules from class 3

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -1,6 +1,10 @@
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class Estacionamento {
-	private static final String CARRO_SAIU_DO_ESTACIONAMENTO = "Carro saiu do estacionamento.";
+	private static final String HORA_DE_ENTRADA_MENOR_QUE_A_HORA_DE_SAÍDA = "Hora de entrada menor que a hora de saída.";
+	private static final String FORA_DE_HORÁRIO = "Fora de horário.";
 	private static final String CARRO_NAO_ESTACIONADO = "Carro não estacionado.";
 	private static final String CARRO_ESTACIONADO_COM_SUCESSO = "Carro estacionado com sucesso.";
 	private static final String ESTACIONAMENTO_LOTADO = "Estacionamento lotado.";
@@ -9,9 +13,17 @@ public class Estacionamento {
 	private static final String REGEX_PLACA_VALIDA = "[A-Z]{3}-\\d{4}";
 	
 	private Carro[] carros;
+	private Date abertura;
+	private Date fechamento;
+	
 	
 	public Estacionamento(int totalDeVagas) {
 		this.carros = new Carro[totalDeVagas];
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, 8);
+		this.abertura = calendar.getTime();
+		calendar.set(Calendar.HOUR_OF_DAY, 22);
+		this.fechamento = calendar.getTime();
 	}
 	
 	public int getVagasDisponiveis() {
@@ -22,7 +34,8 @@ public class Estacionamento {
 		return vagasDisponiveis;
 	}
 
-	public String entrarCarro(Carro carro) {
+	public String entrarCarro(Carro carro, Date horaDeEntrada) {
+		if (!isHorarioFuncionamento(horaDeEntrada)) return FORA_DE_HORÁRIO;
 		if (isCarroEstacionado(carro)) return CARRO_JA_EXISTE;
 		if (!validarPlaca(carro.getPlaca())) return PLACA_INVALIDA;
 		int vagaDisponivel = buscarVagaDisponivel();
@@ -31,6 +44,11 @@ public class Estacionamento {
 		return CARRO_ESTACIONADO_COM_SUCESSO;
 	}
 	
+	private boolean isHorarioFuncionamento(Date hora) {
+		return (hora.after(abertura) && hora.before(fechamento)) || (hora.equals(abertura) || hora.equals(fechamento));
+		
+	}
+
 	private int buscarVagaDisponivel() {
 		for (int numeroDaVaga = 0; numeroDaVaga < carros.length; numeroDaVaga++) {
 			if (carros[numeroDaVaga] == null) return numeroDaVaga;
@@ -38,12 +56,31 @@ public class Estacionamento {
 		return -1;
 	}
 
-	public String sairCarro(Carro carro) {
+	public String sairCarro(Carro carro, Date horaDeEntrada, Date horaDeSaida) {
 		if (!isCarroEstacionado(carro)) return CARRO_NAO_ESTACIONADO;
+		if (!isHorarioFuncionamento(horaDeSaida)) return FORA_DE_HORÁRIO;		
+		if (isHorarioEntradaMenorQueSaida(horaDeEntrada, horaDeSaida)) return HORA_DE_ENTRADA_MENOR_QUE_A_HORA_DE_SAÍDA;
+		
 		carros[buscarVagaDoCarro(carro)] = null;
-		return CARRO_SAIU_DO_ESTACIONAMENTO;
+		
+		return "Carro permaneceu durante " + calcularPermanencia(horaDeEntrada, horaDeSaida) + " horas.";
+		
 	}
 	
+	private int calcularPermanencia(Date horaDeEntrada, Date horaDeSaida) {
+		return getHora(horaDeSaida.getTime()) - getHora(horaDeEntrada.getTime());
+	}
+	
+	private int getHora(long hora){
+		
+		return (int) (hora/1000/60/60);
+		
+	}
+
+	private boolean isHorarioEntradaMenorQueSaida(Date horaDeEntrada, Date horaDeSaida) {
+		return horaDeSaida.before(horaDeEntrada);
+	}
+
 	private int buscarVagaDoCarro(Carro carro) {
 		for (int vagaDoCarro = 0; vagaDoCarro < carros.length; vagaDoCarro++) {
 			if (carro.equals(carros[vagaDoCarro])) return vagaDoCarro;
--- src/EstacionamentoTest.java
+++ src/EstacionamentoTest.java
@@ -1,15 +1,27 @@
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class EstacionamentoTest {
 	private static final int TOTAL_DE_VAGAS = 600;
 	private Estacionamento estacionamento;
+	private Calendar calendar;
+	private Date horaDeEntrada;
+	private Date horaDeSaida;
+	
 
 	@Before
 	public void criarEstacionamento() {
 		estacionamento = new Estacionamento(TOTAL_DE_VAGAS);
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, 9);
+		horaDeEntrada = calendar.getTime();
+		calendar.set(Calendar.HOUR_OF_DAY, 10);
+		horaDeSaida = calendar.getTime();
 	}
 
 	@Test
@@ -20,23 +32,24 @@ public class EstacionamentoTest {
 	
 	@Test
 	public void deveEntrarUmCarro() {
-		estacionamento.entrarCarro(palio());
+		estacionamento.entrarCarro(palio(), horaDeEntrada);
 		int vagasDisponiveis = estacionamento.getVagasDisponiveis();
 		assertEquals(TOTAL_DE_VAGAS - 1, vagasDisponiveis);
 	}
 	
 	@Test
 	public void deveSairUmCarro() {
-		estacionamento.entrarCarro(palio());
-		estacionamento.sairCarro(palio());
+		estacionamento.entrarCarro(palio(), horaDeEntrada);
+		estacionamento.sairCarro(palio(), horaDeEntrada, horaDeSaida);
 		int vagasDisponiveis = estacionamento.getVagasDisponiveis();
 		assertEquals(TOTAL_DE_VAGAS, vagasDisponiveis);
 	}
 	
 	@Test
 	public void naoPodeEntrarCarroDuplicado() {
-		String retornoEstacionamentoSucesso = estacionamento.entrarCarro(palio());
-		String retornoCarroExiste = estacionamento.entrarCarro(palio());
+		
+		String retornoEstacionamentoSucesso = estacionamento.entrarCarro(palio(), horaDeEntrada);
+		String retornoCarroExiste = estacionamento.entrarCarro(palio(), horaDeEntrada);
 		int vagasDisponiveis = estacionamento.getVagasDisponiveis();
 		assertEquals("Carro estacionado com sucesso.", retornoEstacionamentoSucesso);
 		assertEquals("Carro já existe.", retornoCarroExiste);
@@ -46,23 +59,23 @@ public class EstacionamentoTest {
 	@Test
 	public void estacionamentoLotado() {
 		for (int i = 0; i < TOTAL_DE_VAGAS; i++) {
-			String retornoEstacionamentoSucesso = estacionamento.entrarCarro(new Carro("AAA-" + geraPlacaAleatoria(i)));
+			String retornoEstacionamentoSucesso = estacionamento.entrarCarro(new Carro("AAA-" + geraPlacaAleatoria(i)), horaDeEntrada);
 			assertEquals("Carro estacionado com sucesso.", retornoEstacionamentoSucesso);
 		}
-		String retornoEstacionamentoLotado = estacionamento.entrarCarro(new Carro("AAA-" + geraPlacaAleatoria(TOTAL_DE_VAGAS + 1)));
+		String retornoEstacionamentoLotado = estacionamento.entrarCarro(new Carro("AAA-" + geraPlacaAleatoria(TOTAL_DE_VAGAS + 1)), horaDeEntrada);
 		assertEquals("Estacionamento lotado.", retornoEstacionamentoLotado);
 	}
 
 	@Test
 	public void naoPodeSairCarroQueNaoExiste() {
-		estacionamento.entrarCarro(corsa());
-		String retornoCarroNaoExiste = estacionamento.sairCarro(palio());
+		estacionamento.entrarCarro(corsa(), horaDeEntrada);
+		String retornoCarroNaoExiste = estacionamento.sairCarro(palio(), horaDeEntrada, horaDeSaida);
 		assertEquals("Carro não estacionado.", retornoCarroNaoExiste);
 	}
 	
 	@Test
 	public void naoPodeEntrarCarroComPlacaInvalida() {
-		String retornoPlacaInvalida = estacionamento.entrarCarro(new Carro("000-9999"));
+		String retornoPlacaInvalida = estacionamento.entrarCarro(new Carro("000-9999"), horaDeEntrada);
 		assertEquals("Placa inválida.", retornoPlacaInvalida);
 	}
 	
@@ -73,6 +86,43 @@ public class EstacionamentoTest {
 		assertFalse(estacionamento.validarPlaca("000-9999"));
 		assertFalse(estacionamento.validarPlaca("Intelbras"));
 	}
+	
+	@Test
+	public void tentouEntrarUmCarroForaDoHorario(){
+		calendar.set(Calendar.HOUR_OF_DAY, 7);
+		horaDeEntrada = calendar.getTime();		
+		String retornoForaHorario =  estacionamento.entrarCarro(palio(), horaDeEntrada);
+		assertEquals("Fora de horário.", retornoForaHorario);
+	}
+
+	@Test
+	public void tentouSairUmCarroForaDoHorario(){
+		estacionamento.entrarCarro(palio(), horaDeEntrada);
+		calendar.set(Calendar.HOUR_OF_DAY, 23);
+		horaDeSaida = calendar.getTime();
+		String retornoForaHorario =  estacionamento.sairCarro(palio(), horaDeEntrada, horaDeSaida);
+		assertEquals("Fora de horário.", retornoForaHorario);
+	}
+	
+	@Test
+	public void umCarroNaoPodeSairAntesDeTerEntrado(){
+		estacionamento.entrarCarro(palio(), horaDeEntrada);		
+		calendar.set(Calendar.HOUR_OF_DAY, 8);
+		horaDeSaida = calendar.getTime();
+		String retornoForaHorario =  estacionamento.sairCarro(palio(), horaDeEntrada, horaDeSaida);
+		assertEquals("Hora de entrada menor que a hora de saída.", retornoForaHorario);
+	}
+	
+	@Test
+	public void UmCarroPermaneceDurante3HorasNoEstacionamento(){
+		estacionamento.entrarCarro(palio(), horaDeEntrada);
+		calendar.set(Calendar.HOUR_OF_DAY, 12);
+		horaDeSaida = calendar.getTime();
+		String retornoSaidaSucesso =  estacionamento.sairCarro(palio(), horaDeEntrada, horaDeSaida);
+		assertEquals("Carro permaneceu durante 3 horas.", retornoSaidaSucesso);
+	}
+	
+	
 	
 	private Carro palio() {
 		return new Carro("AAA-9999");
